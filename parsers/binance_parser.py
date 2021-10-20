ca526c2f0ba716f63ce4013bebe2fd74694d9d2a
Ivan Ershov <ivan.ershov@glowbyteconsulting.com>
2021-10-20 18:36:51 +0300

    Added create_currencies_datasets

--- parsers/binance_parser.py
+++ parsers/binance_parser.py
@@ -4,7 +4,7 @@ from urllib.parse import urlencode
 from parsers.utils import convert_binance_time_to_normal, convert_normal_time_to_binance
 
 
-def get_currency_data(currency_code: str, year: int):
+def get_currency_data(currency_code: str, year: int) -> [dict]:
     request_url = 'https://api.binance.com/api/v3/klines?'
 
     year_start = str(year) + '-01-01 00:00:00'
@@ -17,7 +17,6 @@ def get_currency_data(currency_code: str, year: int):
         'endTime': convert_normal_time_to_binance(year_end)
     }
 
-    print(request_url + urlencode(request_params))
     response = requests.get(request_url + urlencode(request_params))
 
     if response.status_code != 200:
@@ -26,14 +25,12 @@ def get_currency_data(currency_code: str, year: int):
 
     result = []
     for day_data in response.json():
-        trades_start_time = convert_binance_time_to_normal(day_data[0])
-        trades_end_time = convert_binance_time_to_normal(day_data[6])
+        date = convert_binance_time_to_normal(day_data[0])
         price = day_data[4]
 
         result.append({
-            'trades_start_time': trades_start_time,
-            'trades_end_time': trades_end_time,
-            'price': price
+            'date': date,
+            currency_code: price
         })
 
     return result
